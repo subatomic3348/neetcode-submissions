50fd21245cb12bbac50c19844f06c84e7fd07fc9
Shubham <145556844+subatomic3348@users.noreply.github.com>
2026-08-07 23:42:28 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-1

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& nums) {
+        int profit =0;
+        
+        for(int i =0;i<nums.size()-1;i++){
+            if(nums[i]<nums[i+1]){
+                profit += nums[i+1] - nums[i];
+            }
+        
+        }
+
+        return profit;
+
+        
+    }
+};
