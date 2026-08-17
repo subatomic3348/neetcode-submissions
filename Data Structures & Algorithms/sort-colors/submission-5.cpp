9cc38c6f5ba495a1a10a682539e0f06dbfe771fb
Shubham <145556844+subatomic3348@users.noreply.github.com>
2026-08-17 23:36:39 +0530

    Add: sort-colors - submission-5

---
+++ Data Structures & Algorithms/sort-colors/submission-5.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int zeros = 0;
+        int ones = 0;
+        int twos =0;
+        for(int i =0;i<nums.size();i++){
+            if(nums[i]==0){
+                zeros++;
+            }
+            else if(nums[i]==1){
+                ones++;
+            }
+            else{
+                twos++;
+            }
+        }
+        int i =0;
+        while(zeros--){
+            nums[i] = 0;
+            i++;
+        }
+        while(ones--){
+            nums[i] = 1;
+            i++;
+        }
+        while(twos--){
+            nums[i] = 2;
+            i++;
+        }
+
+        
+    }
+};
