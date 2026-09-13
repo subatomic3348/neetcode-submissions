82e8b5492286708bf70c8ec052dde6e8411e3da8
Shubham <145556844+subatomic3348@users.noreply.github.com>
2026-09-14 00:21:15 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }
+        int freq[26] = {0};
+        for(int i =0;i<s.size();i++){
+            freq[s[i]-'a']++;
+        }
+        for(int i =0;i<t.size();i++){
+            freq[t[i]-'a']--;
+        }
+        for(int i : freq){
+            if(i!=0){
+                return false;
+            }
+        
+        }
+       return true;
+
+    }
+};
